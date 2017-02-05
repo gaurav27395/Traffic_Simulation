7216550eb3fc57e336e0ea1cb7a3d20aa4e205df
abhi3003 <abhijeet.anand804@gmail.com>
2017-02-05 17:59:07 +0530

    Added distribution in macroscopic.py

--- src/M1(Macroscopic).py
+++ src/M1(Macroscopic).py
@@ -1,6 +1,7 @@
 import random
 from threading import Timer
 from Tkinter import *
+import distribution as dis
 
 #Current Direction from which vehicles can move
 allowedDirection="north"
@@ -11,16 +12,22 @@ westQueueLength=0
 northQueueLength=0
 southQueueLength=0
 
+#Arrival times in all the direction.
+arrival_east=0
+arrival_west=0
+arrival_north=0
+arrival_south=0
+
 #Paramters to be used while changing the direction when the traffic timer expires.
 directionArray=["north","east","south","west"]
 directionIndex=0
 directionDictionary={"north":"northQueueLength","east":"eastQueueLength","west":"westQueueLength","south":"southQueueLength"}
 
 #The time for which the signal remains green for one direction
-trafficTimeout=5
+trafficTimeout=6
 
 #The corresponding timers for adding and removing from a queue
-removeTimeout=3
+removeTimeout=2
 addTimeout=1
 
 #The distribution being used to calculate the inter-arrival time of vehicles
@@ -35,22 +42,55 @@ eastQueueLength=random.randint(5,10)
 westQueueLength=random.randint(5,10)
 northQueueLength=random.randint(5,10)
 southQueueLength=random.randint(5,10)
+    
+#Here in arrival_east function we'll generate inter-arrival time and no. of cars on an arrival time
+def arrival_east_fn():
+    scale=3.4
+    iat=dis.exponential_function(scale,1)[0]
+    cars=dis.poisson_function(4,1)[0]
+    global arrival_east,eastQueueLength
+    eastQueueLength+=cars
+    arrival_east+=iat
+    Timer(arrival_east,arrival_east_fn).start()
 
-#The function adds a vehicle to the respective queue whenever the vehicle arrives at the intersection.
-def addToQueue():
-    globals()["eastQueueLength"]+=1
-    globals()["westQueueLength"]+=1
-    globals()["southQueueLength"]+=1
-    globals()["westQueueLength"]+=1
-    Timer(addTimeout,addToQueue).start()
+def arrival_west_fn():
+    scale=3.4
+    iat=dis.exponential_function(scale,1)[0]
+    cars=dis.poisson_function(4,1)[0]
+    global arrival_west,westQueueLength
+    westQueueLength+=cars
+    arrival_west+=iat
+    Timer(arrival_west,arrival_west_fn).start()
+
+def arrival_north_fn():
+    scale=3.4
+    iat=dis.exponential_function(scale,1)[0]
+    cars=dis.poisson_function(4,1)[0]
+    global arrival_north,northQueueLength
+    northQueueLength+=cars
+    arrival_north+=iat
+    Timer(arrival_north,arrival_north_fn).start()
+
+def arrival_south_fn():
+    scale=3.4
+    iat=dis.exponential_function(scale,1)[0]
+    cars=dis.poisson_function(4,1)[0]
+    global arrival_south,southQueueLength
+    southQueueLength+=cars
+    arrival_south+=iat
+    Timer(arrival_south,arrival_south_fn).start()    
+    
+
 
 
 #The function removes a fixed number of vehicles whenever the timer expires
 def removeFromQueue():
     queueVariable=directionDictionary[allowedDirection]
+    global vehiclesCrossingPerTimerExpiry
+    vehiclesCrossingPerTimerExpiry=dis.poisson_function(3,1)[0]
 
     #If vehicles are pressent then make them cross the intersection
-    if globals()[queueVariable]>vehiclesCrossingPerTimerExpiry:
+    if globals()[queueVariable]>=vehiclesCrossingPerTimerExpiry:
         globals()[queueVariable]-=vehiclesCrossingPerTimerExpiry
 
     Timer(removeTimeout,removeFromQueue).start()
@@ -71,7 +111,7 @@ def printInformation():
     print("South Queue Length: ", southQueueLength)
     print("North Queue Length: ", northQueueLength)
     print("***************************************************************\n")
-    Timer(1,printInformation).start()
+    Timer(2,printInformation).start()
 
 
 canvas = Canvas(width=500, height=500, bg='gray11')  
@@ -112,14 +152,17 @@ def line_continuous():
 	canvas.create_line(250, 300, 250, 300+southQueueLength, width=50, fill='brown')
 	south1 = Label(text=""+str(southQueueLength))
 	south1.place(x=240,y=400)
-	Timer(0.5,line_continuous).start()
+	Timer(2,line_continuous).start()
 
 #Begin Simulation Process
 
 #Create 3 different Timer objects for handling 3 different timeouts.
 Timer(trafficTimeout,changeDirection).start()
-Timer(addTimeout,addToQueue).start()
+Timer(arrival_east,arrival_east_fn).start()
+Timer(arrival_west,arrival_west_fn).start()
+Timer(arrival_north,arrival_north_fn).start()
+Timer(arrival_south,arrival_south_fn).start()
 Timer(removeTimeout,removeFromQueue).start()
 printTimer=Timer(1,printInformation).start()
-Timer(0.5,line_continuous).start()
+Timer(2,line_continuous).start()
 mainloop()
