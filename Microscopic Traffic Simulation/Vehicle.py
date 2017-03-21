7fe7a94d3ed20568970a24e80e3a736f03ff4ff7
ankit841 <ankitsingh8341@gmail.com>
2017-03-21 22:54:26 +0530

    Update Vehicle.py

--- Microscopic Traffic Simulation/Vehicle.py
+++ Microscopic Traffic Simulation/Vehicle.py
@@ -1,30 +1,41 @@
 #This module represents a vehicle and its different properties
-import Environment.py
-import Driver.py
-import math
+from Environment import *
+from Driver import *
+
 class Vehicle:
-    def __init__(self,id,typeOfDriver,type,direction,fixed):
+    def __init__(self,id,fixed,typeOfDriver,type,direction):
         self.id=id
         self.type=type
-		self.fixed=fixed
+        self.fixed=fixed
         self.driver=Driver(typeOfDriver)
-        self.appliedPressure=0
+        self.appliedPressure=2
         self.direction=direction
         self.instantaneousAcceleration=self.driver.accelerationCoefficient*self.appliedPressure
         self.instantaneousVelocity=self.driver.accelerationCoefficient*(pow(self.appliedPressure,2))
         self.instantaneousPosition=self.driver.accelerationCoefficient*(pow(self.appliedPressure,3)) 
-        if(direction=="north" or direction=="south")
-            self.position=[fixed,instantaneousPosition]
-        if(direction=="east" or direction=="west")
-            self.position=[instantaneousPosition,fixed]
-            
+        if(direction=="north" or direction=="south"):
+            self.position=[fixed,self.instantaneousPosition]
+        if(direction=="east" or direction=="west"):
+            self.position=[self.instantaneousPosition,fixed]
+                                                                                                      
     def selfSense(self):
-         return {"id":self.id,"type":self.type,"driver":self.Driver,"appliedPressure":self.appliedPressure,"instantaneousAcceleration":self.instantaneousAccelration,"instantaneousVelocity":self.instantaneousVelocity,"instantaneousPosition":self.instantaneousPosition}
+         return {"id":self.id,"type":self.type,"driver":self.driver.type,"appliedPressure":self.appliedPressure,"instantaneousAcceleration":self.instantaneousAcceleration,"instantaneousVelocity":self.instantaneousVelocity,"instantaneousPosition":self.instantaneousPosition}
 
     def getVisibilityRectangle(self):
-        positionMap=Environment.getVehiclePositions()
+        positionMap=getVehiclePositions()
 
-    def getLaneSignalPosition(self):
-        lanePosition = self.currentLane.trafficPosition()
-        selfPosition = self.instantaneousPosition
-        return hypot((lanePosition[0]-selfPosition[0]),(lanePosition[1]-selfPosition[1]))
+    def print(self):
+        print(self.id)
+        print(self.type)
+        print(self.driver.type)
+        print(self.driver.gapLimit)
+        print(self.direction)
+        print(self.instantaneousAcceleration)
+        print(self.instantaneousVelocity)
+        print(self.instantaneousPosition)
+        print(self.position)
+
+v1=Vehicle(24244,-2,"aggressive","car","north")
+a=v1.print()
+a=v1.selfSense()
+print(a)
