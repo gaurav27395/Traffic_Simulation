bfe78a6afb6589213b720d0c625d49ec037c9b01
Kishan Kishore <kishan2695@gmail.com>
2017-03-24 00:51:52 +0530

    Update Controller.py

--- Microscopic_Traffic_Simulation/Controller.py
+++ Microscopic_Traffic_Simulation/Controller.py
@@ -1,41 +1,43 @@
 #Controls the operation of a vehicle
-from Driver import *
-from Environment import *
-from Behaviour import *
-from Vehicle import *
+
+from Microscopic.Driver import *
+from Microscopic.Environment import *
+from Microscopic.Behaviour import *
+from Microscopic.Vehicle import *
 from threading import Timer
 
-vehicle=Vehicle("1234",3,"aggressive","bike","northRight")
-vehicle1=Vehicle("1234",14,"aggressive","car","southRight")
-vehicle2=Vehicle("1234",3,"aggressive","truck","eastUp")
-vehicle3=Vehicle("1234",8,"aggressive","bike","westDown")
-# Move already,will you?
+vehicle=Vehicle("1234",3,"aggressive","bike","southLeft")
+vehicle1=Vehicle("1234",14,"aggressive","car","northRight")
+vehicle2=Vehicle("1234",3,"aggressive","truck","eastDown")
+vehicle3=Vehicle("1234",8,"aggressive","bike","westUp")
+
 
 def a():
     vehicle.pressAcceleration()
     Timer(1,a).start()
     print("first: "+str(vehicle.position))
-    print("Generate All Score: " + generateAllScore(vehicle))
+    print("Generate All Score 1 : " + generateAllScore(vehicle)+" \n")
 
 def b():
     vehicle1.pressAcceleration()
     Timer(1,b).start()
     print("second: "+str(vehicle1.position))
-    print("Generate All Score: " + generateAllScore(vehicle1))
+    print("Generate All Score 2 : " + generateAllScore(vehicle1)+" \n")
 
 def c():
     vehicle2.pressAcceleration()
     Timer(1,c).start()
     print("third: "+str(vehicle2.position))
-    print("Generate All Score: " + generateAllScore(vehicle2))
+    print("Generate All Score 3 : " + generateAllScore(vehicle2)+" \n")
 
 def d():
     vehicle3.pressAcceleration()
     Timer(1,d).start()
     print("fourth: "+str(vehicle3.position))
-    print("Generate All Score: " + generateAllScore(vehicle3))
+    print("Generate All Score 4 : " + generateAllScore(vehicle3)+" \n")
+
 Timer(1,a).start()
-Timer(1,b).start()
+Timer(2,b).start()
 Timer(1,c).start()
-Timer(1,d).start()
+Timer(2,d).start()
 
