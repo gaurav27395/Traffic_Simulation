c0cea0c63202d8a20884304b161b18fccb264f09
Kishan Kishore <kishan2695@gmail.com>
2017-04-09 23:57:04 +0530

    Update Controller.py

--- Microscopic/Controller.py
+++ Microscopic/Controller.py
@@ -1,9 +1,9 @@
 #Controls the operation of a vehicle
 
-from Driver import *
-from Environment import *
-from Behaviour import *
-from Vehicle import *
+from Microscopic.Driver import *
+from Microscopic.Environment import *
+from Microscopic.Behaviour import *
+from Microscopic.Vehicle import *
 from threading import Timer
 import tkinter
 
@@ -66,26 +66,52 @@ def introduceNewVehicle(direction=None,type=None):
     vehicle=createVehicle(direction,type)
     vehicle.start()
     vehicle.printVehicleInformation()
+    addVehicleToGUI(vehicle)
+
+def addVehicleToGUI(vehicle):
+    xposition = vehicle.position[0] * 1.625 + 400
+    yposition = vehicle.position[0] * 0.975 + 400
+
+    if vehicle.type == 'car':
+        canvas.create_rectangle(xposition, yposition,xposition+10, yposition+5, outline='blue', fill='blue')
+    elif vehicle.type == 'bus':
+        canvas.create_rectangle(xposition, yposition,xposition+20, yposition+10, outline='blue', fill='blue')
+    elif vehicle.type == 'bike':
+        canvas.create_rectangle(xposition, yposition,xposition+8, yposition+3, outline='blue', fill='blue')
+
+
 
 #The following line of code are used for creating the input GUI
 window=tkinter.Tk()
-window.geometry("300x300")
+window.geometry("1300x800")
 
-simulationLabel=tkinter.Label(window,text="Traffic Simulation",font=("Helvetica", 20))
-simulationLabel.pack()
+
+canvas = tkinter.Canvas(window,width=1300, height=800, bg='gray11')
+canvas.pack()
+canvas.create_rectangle(550, 300, 750, 460, fill='gray26')
+canvas.create_rectangle(0, 0, 550, 300, fill='navajo white')
+canvas.create_rectangle(0, 460, 550, 800, fill='navajo white')
+canvas.create_rectangle(750, 460, 1300, 800, fill='navajo white')
+canvas.create_rectangle(750, 0, 1300, 300, fill='navajo white')
+canvas.create_rectangle(550, 0, 750, 300, fill='grey') #North
+canvas.create_rectangle(0, 300, 550, 460, fill='grey') #West
+canvas.create_rectangle(550, 460, 750, 800, fill='grey') #South
+canvas.create_rectangle(750, 300, 1300, 460, fill='grey') #East
+
+simulationLabel=tkinter.Label(canvas,text="Traffic Simulation",font=("Helvetica", 20))
+simulationLabel.place(x=20,y=20)
 
 directionVariable=tkinter.StringVar(window)
 directionVariable.set("Random")
 directionMenu = tkinter.OptionMenu(window,directionVariable,"Random","northRight", "southLeft", "eastDown","westUp")
-directionMenu.pack()
-
+directionMenu.place(x=20,y=60)
 
 typeVariable=tkinter.StringVar(window)
 typeVariable.set("Random")
 typeMenu=tkinter.OptionMenu(window,typeVariable,"Random","car","bus","bike")
-typeMenu.pack()
+typeMenu.place(x=120,y=60)
 
-button=tkinter.Button(window,text="ADD VEHICLE",command=lambda: introduceNewVehicle(directionVariable.get(),typeVariable.get()))
-button.pack()
+button=tkinter.Button(window,text="ADD VEHICLE",bg="white",command=lambda: introduceNewVehicle(directionVariable.get(),typeVariable.get()))
+button.place(x=250,y=60)
 
 window.mainloop()
