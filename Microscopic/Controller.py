f7cc02cab05e6847c90f480d08b5cfc468fc6ead
Kishan Kishore <kishan2695@gmail.com>
2017-04-09 17:29:59 +0530

    Update Controller.py

--- Microscopic/Controller.py
+++ Microscopic/Controller.py
@@ -5,20 +5,27 @@ from Environment import *
 from Behaviour import *
 from Vehicle import *
 from threading import Timer
+import tkinter
 
 totalVehicleCount=0
 
 
-def createVehicle():
+def createVehicle(vehicleDirection,vehicleType):
     global totalVehicleCount
     totalVehicleCount+=1
     id=totalVehicleCount+1
-    type=["bike","car","truck"][random.randint(0,2)]
-    direction=["northRight","southLeft","eastDown","westUp"][random.randint(0,3)]
+
+    if vehicleDirection=="Random":
+        direction=["northRight","southLeft","eastDown","westUp"][random.randint(0,3)]
+    else:
+        direction=vehicleDirection
+
+    if vehicleType=="Random":
+        type = ["bike", "car", "truck"][random.randint(0, 2)]
+    else:
+        type=vehicleType
+
     fixed=0
-
-    
-
     if direction=="northRight" or direction=="westUp":
         if type=="bike":
             fixed=random.randint(5,5)
@@ -52,12 +59,33 @@ def createVehicle():
         newVehicle.turnLeft = False
 
     addVehicleToEnvironment(id,newVehicle)
+    print("A new vehicle was created ")
     return newVehicle
 
-vehicle=createVehicle()
-vehicle.start()
-vehicle.printVehicleInformation()
-vehicle1=createVehicle()
-vehicle1.start()
-vehicle1.printVehicleInformation()
-print("Status Map:"+str(vehicleStatusMap))
+def introduceNewVehicle(direction=None,type=None):
+    vehicle=createVehicle(direction,type)
+    vehicle.start()
+    vehicle.printVehicleInformation()
+
+#The following line of code are used for creating the input GUI
+window=tkinter.Tk()
+window.geometry("300x300")
+
+simulationLabel=tkinter.Label(window,text="Traffic Simulation",font=("Helvetica", 20))
+simulationLabel.pack()
+
+directionVariable=tkinter.StringVar(window)
+directionVariable.set("Random")
+directionMenu = tkinter.OptionMenu(window,directionVariable,"Random","northRight", "southLeft", "eastDown","westUp")
+directionMenu.pack()
+
+
+typeVariable=tkinter.StringVar(window)
+typeVariable.set("Random")
+typeMenu=tkinter.OptionMenu(window,typeVariable,"Random","Car","Bus","Bike")
+typeMenu.pack()
+
+button=tkinter.Button(window,text="ADD VEHICLE",command=lambda: introduceNewVehicle(directionVariable.get(),typeVariable.get()))
+button.pack()
+
+window.mainloop()
